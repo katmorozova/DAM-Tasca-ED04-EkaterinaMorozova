84d085ceb3f9b4f62b03f37d0c5b7c2696a1bde1
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2026-02-09 14:36:49 +0100

    Añadidos comentarios Javadoc a clase Llibre

--- src/biblioteca/Main.java
+++ src/biblioteca/Main.java
@@ -1,10 +1,34 @@
 package biblioteca;
 
+/**
+ *  Clase Main que gestiona el préstamo y la devolución
+ *  de libros en una biblioteca mediante la creación
+ *  y manipulación de objetos de la clase Llibre.
+ *
+ *  Ejecuta un ejemplo práctico de uso.
+ *
+ * @author Ekaterina Morozova
+ */
+
 public class Main {
     public static void main(String[] args) {
+
+        /**
+         * Método principal del programa
+         * Inicia la gestión del libro llamando al método gestionarLlibre
+         *
+         * @param args -> argumentos de línea de comandos (no utilizados)
+         */
         gestionarLlibre(4, 2);
     }
 
+    /**
+     * Gestiona las operaciones principales sobre un libro:
+     * muestra sus datos, presta ejemplares y devuelve ejemplares.
+     *
+     * @param quantitatAPrestar -> cantidad de ejemplares a prestar
+     * @param quantitatARetornar -> cantidad de ejemplares a devolver
+     */
     private static void gestionarLlibre(int quantitatAPrestar, int quantitatARetornar) {
         Llibre llibrePrincipal = new Llibre("Tirant lo Blanc", "Joanot Martorell", 10, 19.95f);
 
